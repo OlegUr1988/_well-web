0008c375c0f8819157976258875381009f2d6ee5
Doniyor Alimov <alimov.doniyor28@gmail.com>
2023-11-24 11:45:51 +0500

    Installing React Query

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,21 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import router from "./router.tsx";
 import theme from "./theme.ts";
 
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={client}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
