b56fd8001f3dfec5623bf6436e5ad347f2a9ce80
Doniyor Alimov <alimov.doniyor28@gmail.com>
2023-12-26 09:34:04 +0500

    Issue 29 - Add PHD tags import and export buttons

--- src/components/PHDTags/PHDTagsCommandPanel.tsx
+++ src/components/PHDTags/PHDTagsCommandPanel.tsx
@@ -1,8 +1,23 @@
+import { useImportPHDTagsFromExcel } from "../../hooks/PHDTags";
 import CommandPanel from "../CommandPanel";
+import ExportButton from "../ExportButton";
+import ImportButton from "../ImportButton";
 
 const PHDTagsCommandPanel = () => {
+  const { mutateAsync } = useImportPHDTagsFromExcel();
   return (
-    <CommandPanel createPath="/config/phd-tags/new" createLabel="Create" />
+    <CommandPanel
+      createPath="/config/phd-tags/new"
+      createLabel="Create"
+      exportButton={<ExportButton url="/phd-tags/exportToExcel" />}
+      importButton={
+        <ImportButton
+          mutateAsync={mutateAsync}
+          successMessage="PHD tags were updated"
+          routeAfterImport="/config/phd-tags"
+        />
+      }
+    />
   );
 };
 
