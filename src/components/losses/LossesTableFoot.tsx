b9a9d4815d31e9db2998b3b9e2fe1cad5ad414dc
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-01-17 09:52:58 +0500

    Issue 71 - Add calculation function

--- src/components/losses/LossesTableFoot.tsx
+++ src/components/losses/LossesTableFoot.tsx
@@ -2,7 +2,7 @@ import { Tfoot, Th, Tr } from "@chakra-ui/react";
 import _ from "lodash";
 import { Attribute } from "../../entities/attributes";
 import useGetRecords from "../../hooks/useGetRecords";
-import { getRecordsByUnits, getSumOfRecords } from "../../utils/records";
+import { calculateRecordsSum } from "../../utils/records";
 
 interface Props {
   attributes: Attribute[];
@@ -19,19 +19,13 @@ const LossesTableFoot = ({ attributes, label = "Total" }: Props) => {
     <Tfoot>
       <Tr>
         <Th>{label}</Th>
-        <Th textAlign="center">
-          {getSumOfRecords(getRecordsByUnits(records!, "kWh"))}
-        </Th>
+        <Th textAlign="center">{calculateRecordsSum(records!, "kWh")}</Th>
         <Th textAlign="center">kWh</Th>
-        <Th textAlign="center">
-          {getSumOfRecords(getRecordsByUnits(records!, "ton CO2"))}
-        </Th>
+        <Th textAlign="center">{calculateRecordsSum(records!, "ton CO2")}</Th>
         <Th textAlign="center" whiteSpace="nowrap">
           Ton CO2
         </Th>
-        <Th textAlign="center">
-          {getSumOfRecords(getRecordsByUnits(records!, "%")) + "%"}
-        </Th>
+        <Th textAlign="center">{calculateRecordsSum(records!, "%") + "%"}</Th>
       </Tr>
     </Tfoot>
   );
--- src/components/losses/LossesTableRow.tsx
+++ src/components/losses/LossesTableRow.tsx
@@ -1,7 +1,7 @@
 import { Td, Tr } from "@chakra-ui/react";
 import { Attribute } from "../../entities/attributes";
 import useGetRecords from "../../hooks/useGetRecords";
-import { getRecordsByUnits, getSumOfRecords } from "../../utils/records";
+import { calculateRecordsSum } from "../../utils/records";
 
 const LossesTableRow = ({ attribute }: { attribute: Attribute }) => {
   const { records } = useGetRecords(attribute.assignment);
@@ -11,20 +11,13 @@ const LossesTableRow = ({ attribute }: { attribute: Attribute }) => {
   return (
     <Tr key={attribute.id}>
       <Td>{attribute.name}</Td>
-      <Td textAlign="center">
-        {getSumOfRecords(getRecordsByUnits(records!, "kWh"))}
-      </Td>
+      <Td textAlign="center">{calculateRecordsSum(records!, "kWh")}</Td>
       <Td textAlign="center">kWh</Td>
-      <Td textAlign="center">
-        {" "}
-        {getSumOfRecords(getRecordsByUnits(records!, "ton CO2"))}
-      </Td>
+      <Td textAlign="center"> {calculateRecordsSum(records!, "ton CO2")}</Td>
       <Td textAlign="center" whiteSpace="nowrap">
         Ton CO2
       </Td>
-      <Td textAlign="center">
-        {getSumOfRecords(getRecordsByUnits(records!, "%")) + "%"}
-      </Td>
+      <Td textAlign="center">{calculateRecordsSum(records!, "%") + "%"}</Td>
     </Tr>
   );
 };
