4c7e1a01912703eb4ed92351e73a66c6c908fe2d
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-02-05 14:30:13 +0500

    Issue 75 - Add select input

--- src/components/dashboards/DashboardAssetSelectInput.tsx
+++ src/components/dashboards/DashboardAssetSelectInput.tsx
@@ -1,12 +1,43 @@
-import { Tag, Text, VStack } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
+import { Select } from "chakra-react-select";
+import { useNavigate } from "react-router-dom";
 import { DashboardCard } from ".";
+import { Area } from "../../entities/areas";
+import { Asset } from "../../entities/assets";
+import { useAssets } from "../../hooks/assets";
 
-const DashboardAssetSelectInput = ({ assetName }: { assetName: string }) => {
+const DashboardAssetSelectInput = ({
+  area,
+  asset,
+}: {
+  area: Area;
+  asset: Asset;
+}) => {
+  const { data: assets, isLoading, error } = useAssets({});
+  const navigate = useNavigate();
+
+  if (isLoading) return null;
+
+  if (error) return null;
+
+  const options = assets?.map((asset) => ({
+    value: asset.id,
+    label: asset.name,
+  }));
+
+  const defaultValue = { value: asset.id, label: asset.name };
+
   return (
     <DashboardCard>
       <VStack gap={0} align="start" rowGap={1}>
         <Text fontSize={16}>Select Asset:</Text>
-        <Tag>{assetName}</Tag>
+        <Select
+          defaultValue={defaultValue}
+          onChange={(option) =>
+            navigate(`/dashboards/${area.name}/${option?.label}`)
+          }
+          options={options}
+        />
       </VStack>
     </DashboardCard>
   );
