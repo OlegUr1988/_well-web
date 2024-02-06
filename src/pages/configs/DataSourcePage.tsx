86bb35bb15aa9d2dd1ed3967fbc8c6b72b10a555
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-02-06 13:13:42 +0500

    Issue 90 - Add data source page

---
+++ src/pages/configs/DataSourcePage.tsx
@@ -0,0 +1,11 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+const DataSourcePage = () => {
+  return (
+    <Container maxW="container.xl">
+      <Heading>Data Source Configuration</Heading>
+    </Container>
+  );
+};
+
+export default DataSourcePage;
--- src/pages/configs/index.ts
+++ src/pages/configs/index.ts
@@ -1,2 +1,4 @@
+export { default as DataSourcePage } from './DataSourcePage';
 export { default as PHDTagsPage } from "./PHDTagsPage";
 export { default as UnitsPage } from "./UnitsPage";
+
