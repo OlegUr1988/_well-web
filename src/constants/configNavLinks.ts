4a34f007321cf3ed966cede596ac8e49567bbb93
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-03-03 20:14:02 +0500

    Issue 99 - Add link to change password

--- src/constants/configNavLinks.ts
+++ src/constants/configNavLinks.ts
@@ -1,21 +1,31 @@
 import { CiDatabase } from "react-icons/ci";
 import { TbRulerMeasure } from "react-icons/tb";
 import { TiTags } from "react-icons/ti";
+import { IoKeyOutline } from "react-icons/io5";
 
 export default [
   {
     name: "Units",
     url: "/config/units",
     icon: TbRulerMeasure,
+    isAuthRequired: false,
   },
   {
     name: "PHD Tags",
     url: "/config/tags",
     icon: TiTags,
+    isAuthRequired: false,
   },
   {
     name: "Data Source",
     url: "/config/datasource",
     icon: CiDatabase,
+    isAuthRequired: false,
+  },
+  {
+    name: "Change Password",
+    url: "/config/change-password",
+    icon: IoKeyOutline,
+    isAuthRequired: true,
   },
 ];
