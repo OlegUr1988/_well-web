48c5bf62bd34be4ed9de1410a27301241fd2a050
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-03-03 21:21:15 +0500

    Issue 99 - Handle success scenario

--- src/components/users/ChangeUserPasswordForm.tsx
+++ src/components/users/ChangeUserPasswordForm.tsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { ChangeUserPasswordFormData } from "../../entities/formDatas";
 import { useFormSubmit } from "../../hooks/forms";
 import useChangeUserPassword from "../../hooks/users/useChangeUserPassword";
+import { clearToken } from "../../utils/auth";
 import { changeUserPasswordSchema } from "../../validationSchema";
 import { FormContainer, FormInput, FormSubmit } from "../forms";
 
 const ChangeUserPasswordForm = () => {
   const { mutateAsync, isPending } = useChangeUserPassword();
+  const navigate = useNavigate();
+
+  const handleOnSuccess = () => {
+    clearToken();
+    navigate("/login");
+  };
+
   const { register, handleSubmit, onSubmit, errors } = useFormSubmit<
     ChangeUserPasswordFormData,
     ChangeUserPasswordFormData
@@ -13,6 +22,7 @@ const ChangeUserPasswordForm = () => {
     onSuccessMessage: "Password was successfuly updated",
     mutateAsync,
     schema: changeUserPasswordSchema,
+    onSuccess: handleOnSuccess,
   });
 
   return (
