35e449ba51760de06b48e07c6fa240495ce5ec8c
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-03-04 18:47:52 +0500

    Issue 100 - Add pagination

--- src/pages/configs/UsersPage.tsx
+++ src/pages/configs/UsersPage.tsx
@@ -2,6 +2,7 @@ import { Box, Container, Heading } from "@chakra-ui/react";
 import { UsersList } from "../../components/users";
 import UserCommandPanel from "../../components/users/UserCommandPanel";
 import UserSearchInput from "../../components/users/UserSearchInput";
+import UsersPagination from "../../components/users/UsersPagination";
 
 const UsersPage = () => {
   return (
@@ -20,6 +21,8 @@ const UsersPage = () => {
         <Box mb={5}>
           <UsersList />
         </Box>
+
+        <UsersPagination />
       </Container>
     </>
   );
