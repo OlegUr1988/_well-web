56e0a654ec6b2f60feedfcfaf353684aaa1271ce
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-03-04 10:42:35 +0500

    Issue 103 - Change sidebar

--- src/pages/ConfigPanelPage.tsx
+++ src/pages/ConfigPanelPage.tsx
@@ -1,16 +1,12 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
-import BreadcrumbBar from "../components/common/BreadcrumbBar";
 
 const ConfigPanelPage = () => {
   return (
     <Flex direction={"row"} height="100%">
       <SideBar />
       <Flex direction={"column"} flex={1}>
-        <Box>
-          <BreadcrumbBar />
-        </Box>
         <Box p={3} overflowY="auto" height="100%">
           <Outlet />
         </Box>
