8f0aba5f06720f2fbb51aca05f7f2e4e640d8f94
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-03-26 17:08:35 +0500

    Issue 137 -Update area level dashboard

--- src/pages/dashboards/AreaLevelDashboard.tsx
+++ src/pages/dashboards/AreaLevelDashboard.tsx
@@ -5,14 +5,21 @@ import AreaLossesCard from "../../components/dashboards/AreaLossesCard";
 import AreaTotalCards from "../../components/dashboards/AreaTotalCards";
 import LoadingSpinner from "../../components/models/LoadingSpinner";
 import { useAssetByName } from "../../hooks/assets";
+import useUtilityTypes from "../../hooks/useUtilityTypes";
 
 const AreaLevelDashboard = () => {
   const { areaName } = useParams();
+  const { data: types } = useUtilityTypes();
   const { data: area, isLoading, error } = useAssetByName({ name: areaName });
+
+  const areaType = types?.find((type) => type.name.toLowerCase() === "area");
 
   if (isLoading) return <LoadingSpinner />;
 
   if (error) return <Heading>Invalid area name provided</Heading>;
+
+  if (area?.utilityTypeId !== areaType?.id)
+    return <Heading>Invalid asset type provided</Heading>;
 
   return (
     <Box p={5}>
