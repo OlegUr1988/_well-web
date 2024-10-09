d71fa3e7c1b697d61476cdfd783e1610d2ef5523
Doniyor Alimov <alimov.doniyor28@gmail.com>
2024-10-09 13:13:15 +0500

    Issue 207 - Update formats

--- src/components/dashboards/areas/AreaTotalCards.tsx
+++ src/components/dashboards/areas/AreaTotalCards.tsx
@@ -1,4 +1,4 @@
-import { Box, GridItem, SimpleGrid } from "@chakra-ui/react";
+import { GridItem, SimpleGrid } from "@chakra-ui/react";
 import { Asset } from "../../../entities/assets";
 import { useCalculateKPI } from "../../../hooks/calculations";
 import useDashboardsStore from "../../../store/dashboard";
@@ -40,51 +40,47 @@ const AreaTotalCards = ({ area }: { area: Asset }) => {
   } = targetDifferences;
 
   return (
-    <Box>
-      <SimpleGrid columns={4} gap={5}>
-        <GridItem onClick={() => setTrend("production")} cursor="pointer">
-          <TotalKPICard
-            header="Production"
-            value={totalProduction}
-            units={productionUnit}
-            difference={productionTargetDifference}
-          />
-        </GridItem>
-        <GridItem
-          onClick={() => setTrend("energy consumption")}
-          cursor="pointer"
-        >
-          <TotalKPICard
-            header="Energy Consumption"
-            value={totalEnergyConsumption}
-            units={energyConsumptionUnit}
-            difference={energyConsumptDifference}
-            isLimit={true}
-          />
-        </GridItem>
-        <GridItem
-          onClick={() => setTrend("specific energy consumption")}
-          cursor="pointer"
-        >
-          <TotalKPICard
-            header="Specific Energy Consumption"
-            value={totalSpecificEnergyConsumption}
-            units="kWh/Ton"
-            difference={specificEnergyConsumptionDifference}
-            isLimit={true}
-          />
-        </GridItem>
-        <GridItem onClick={() => setTrend("CO2 emission")} cursor="pointer">
-          <TotalKPICard
-            header="CO2 Emission"
-            value={totalCO2Emission}
-            units="Ton"
-            difference={CO2EmissionDifference}
-            isLimit={true}
-          />
-        </GridItem>
-      </SimpleGrid>
-    </Box>
+    <SimpleGrid columns={4} gap={5}>
+      <GridItem onClick={() => setTrend("production")} cursor="pointer">
+        <TotalKPICard
+          header="Production"
+          value={totalProduction}
+          units={productionUnit}
+          difference={productionTargetDifference}
+        />
+      </GridItem>
+      <GridItem onClick={() => setTrend("energy consumption")} cursor="pointer">
+        <TotalKPICard
+          header="Energy Consumption"
+          value={totalEnergyConsumption}
+          units={energyConsumptionUnit}
+          difference={energyConsumptDifference}
+          isLimit={true}
+        />
+      </GridItem>
+      <GridItem
+        onClick={() => setTrend("specific energy consumption")}
+        cursor="pointer"
+      >
+        <TotalKPICard
+          header="Specific Energy Consumption"
+          value={totalSpecificEnergyConsumption}
+          units="kWh/Ton"
+          difference={specificEnergyConsumptionDifference}
+          isLimit={true}
+          showDigits={true}
+        />
+      </GridItem>
+      <GridItem onClick={() => setTrend("CO2 emission")} cursor="pointer">
+        <TotalKPICard
+          header="CO2 Emission"
+          value={totalCO2Emission}
+          units="Ton"
+          difference={CO2EmissionDifference}
+          isLimit={true}
+        />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
